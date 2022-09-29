dee3d81f628382456de5160e4bdbfe5b6a90bef5
Tushar985 <tusharmaity989@gmail.com>
2022-09-29 23:17:33 +0530

    Initial Commit with installed dependencies & setup

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="h-screen w-full bg-black text-white flex items-center justify-center text-3xl">
+        Home
+    </div>
+  )
+}
+
+export default Home
